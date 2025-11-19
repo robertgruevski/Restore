abe2b4e97bdafdcb63d60da6cdb555da0507f3de
robertgruevski <robertgruevski@outlook.com>
2025-11-19 17:25:11 -0500

    55. Implementing RTK Query part 2

---
+++ client/src/app/store/store.ts
@@ -0,0 +1,21 @@
+import { configureStore, legacy_createStore } from "@reduxjs/toolkit";
+import counterReducer, {
+	counterSlice,
+} from "../../features/contact/counterReducer";
+import { useDispatch, useSelector } from "react-redux";
+
+export function configureTheStore() {
+	return legacy_createStore(counterReducer);
+}
+
+export const store = configureStore({
+	reducer: {
+		counter: counterSlice.reducer,
+	},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
