da3ea48eb46fecd9bce708a7db4d4f80a6587ffe
robertgruevski <robertgruevski@outlook.com>
2025-12-09 17:44:53 -0500

    105. Updating the catalog component to use the filters

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 using API.Data;
 using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +10,19 @@ namespace API.Controllers
     public class ProductsController(StoreContext context) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
-            return await context.Products.ToListAsync();
+            var query = context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.Metadata);
+
+            return products;
         }
 
         [HttpGet("{id}")]
@@ -19,6 +31,15 @@ namespace API.Controllers
             var product = await context.Products.FindAsync(id);
 
             return product is null ? NotFound() : product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFitlers()
+        {
+            var brands = await context.Products.Select(x => x.Brand).Distinct().ToListAsync();
+            var types = await context.Products.Select(x => x.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
         }
     }
 }
