41b15fa0a75010991612b39d21efc0e9d28e3804
robertgruevski <robertgruevski@outlook.com>
2025-12-11 17:17:44 -0500

    117. Adding the entity configuration for identity

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,10 +1,32 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Product> Products { get; set; }
     public required DbSet<Basket> Baskets { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole 
+                { 
+                    Id = "ea1df714-7934-4f65-8edd-32ef32afeaed", 
+                    Name = "Member", 
+                    NormalizedName = "MEMBER" 
+                },
+                new IdentityRole 
+                { 
+                    Id = "60bd8b8b-6b5c-48f8-9124-fa535bfa0d33", 
+                    Name = "Admin", 
+                    NormalizedName = "ADMIN" 
+                }
+            );
+    }
 }
